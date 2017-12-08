17f904c60be9d2a096ed41eed27ccbd297e63a70
int13hfv <34365338+int13hfv@users.noreply.github.com>
2017-12-08 10:06:33 +0300

    Create add_box.sh

---
+++ add_box.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Script for mass creating mailboxes in ISP Manager
+# syntax to run ./add_box.sh mailboxes.txt
+# 
+# mailboxes.txt syntax:
+# 	
+# username@domainname.tld somepassword
+#
+
+{ while IFS=' ' read  mailbox password ; do
+	
+	domainname=${mailbox##*@} 
+	username=${mailbox%@*} 
+
+  /usr/local/mgr5/sbin/mgrctl\
+	-m ispmgr email.edit\
+  domainname=$domainname\
+  greylist=on hidegreylist=on\
+  passwd=$password name=$username\
+  sok=ok
+
+done ; } < $1
